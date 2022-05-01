d2d7efba12d9591790156a79a412e561f37f7b12
Aye Khat Khat Khaing <ayekhatkhatkhaingak3344@gmail.com>
2022-05-01 22:41:15 +0630

    added-carousel

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-let slideIndex = 1;
-let btnStatus = true;
-showSlides(slideIndex);
+let index = 1;
+showImageSlides(index);
 
 // Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusImageSlides(n) {
+    showImageSlides(index += n);
     fadeInDown()
 }
 function minusSlides(n) {
-    showSlides(slideIndex += n);
+    showImageSlides(index += n);
     fadeInUp();
 }
-function showSlides(n) {
+function showImageSlides(n) {
     let i;
     let slides = document.querySelectorAll(".slide-left");
     if (n > slides.length) {
-        slideIndex = 1;
+        index = 1;
     }
     if (n < 1) {
-        slideIndex = slides.length;
+        index = slides.length;
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
+    slides[index - 1].style.display = "block";
 
     let j;
     let rslides = document.querySelectorAll(".slide-right");
     if (n > rslides.length) {
-        slideIndex = 1;
+        index = 1;
     }
     if (n < 1) {
-        slideIndex = rslides.length;
+        index = rslides.length;
     }
     for (j = 0; j < rslides.length; j++) {
         rslides[j].style.display = "none";
     }
-    rslides[slideIndex - 1].style.display = "block";
+    rslides[index - 1].style.display = "block";
 
 
 }
@@ -69,3 +68,91 @@ function fadeInDown() {
     });
     // console.log("down");
 }
+
+//blur background
+let bg = document.getElementsByClassName("card");
+let slides = document.getElementsByClassName("mySlides");
+let blur = document.getElementById("blur");
+let slideIndex = 1;
+function active() {
+    blur.classList.toggle('active');
+}
+for (e in bg) {
+    bg[e].addEventListener("click", function () {
+        // slides.classList.toggle('active');
+        active();
+        document.body.innerHTML += `
+        <div class="slideshow-container">
+            <div class="mySlides fade">
+                <div class="numbertext">1 / 6</div>
+                <img src="./images/background.jpg" style="width:100%;height: 350px;">
+                <button class="text" id="close" onclick="closeGallery()">close</button>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">2 / 6</div>
+                <img src="./images/background1.jpg" style="width:100%;height: 350px;">
+                <button class="text" id="close" onclick="closeGallery()">close</button>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">3 / 6</div>
+                <img src="./images/background2.jpg" style="width:100%;height: 350px;">
+                <button class="text" id="close" onclick="closeGallery()">close</button>
+            </div>
+            <div class="mySlides fade">
+                <div class="numbertext">4 / 6</div>
+                <img src="./images/cm-1.jpg" style="width:100%;height: 350px;">
+                <button class="text" id="close" onclick="closeGallery()">close</button>
+            </div>
+            <div class="mySlides fade">
+                <div class="numbertext">5 / 6</div>
+                <img src="./images/cover1.jpg" style="width:100%;height: 350px;">
+                <button class="text" id="close" onclick="closeGallery()">close</button>
+            </div>
+            <div class="mySlides fade">
+                <div class="numbertext">6 / 6</div>
+                <img src="./images/cover2.jpg" style="width:100%;height: 350px;">
+                <button class="text" id="close" onclick="closeGallery()">close</button>
+            </div>
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        `;
+
+        showSlides(slideIndex);
+        buttonVisibility();
+    })
+}
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function buttonVisibility() {
+    document.querySelector('.prev').style.display = "block";
+    document.querySelector('.next').style.display = "block";
+}
+
+function closeGallery() {
+    let blur = document.querySelector('.card-list');
+    let last = document.querySelector('.slideshow-container');
+    document.body.removeChild(last);
+    blur.classList.toggle('active')
+}
+
